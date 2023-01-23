271cc9162fac15a9c52f3b6a3bc76b2e576bd14d
Chris Pulman <chris.pulman@yahoo.com>
2023-01-23 00:36:23 +0000

    Add HashTableRx functions

--- src/MQTTnet.Rx.TwinCAT/Create.cs
+++ src/MQTTnet.Rx.TwinCAT/Create.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Reactive.Linq;
+using CP.Collections;
 using CP.TwinCatRx;
 using MQTTnet.Client;
 using MQTTnet.Extensions.ManagedClient;
@@ -46,6 +47,37 @@ namespace MQTTnet.Rx.TwinCAT
         }
 
         /// <summary>
+        /// Publishes the ab PLC tag.
+        /// </summary>
+        /// <typeparam name="T">>The PLC Tag Data Type.</typeparam>
+        /// <param name="client">The client.</param>
+        /// <param name="topic">The topic.</param>
+        /// <param name="plcVariable">The PLC variable.</param>
+        /// <param name="configurePlc">The configure PLC.</param>
+        /// <returns>MqttClientPublishResult.</returns>
+        public static IObservable<MqttClientPublishResult> PublishTcPlcTag<T>(this IObservable<IMqttClient> client, string topic, string plcVariable, Action<IHashTableRx> configurePlc)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (configurePlc == null)
+            {
+                throw new ArgumentNullException(nameof(configurePlc));
+            }
+
+            var plc = default(HashTableRx)!;
+            configurePlc(plc);
+            if (plc == null)
+            {
+                throw new ArgumentNullException(nameof(plc));
+            }
+
+            return client.PublishMessage(plc.Observe<T>(plcVariable).Select(payLoad => (topic, payLoad: payLoad!.ToString()!)));
+        }
+
+        /// <summary>
         /// Publishes the tc PLC tag.
         /// </summary>
         /// <typeparam name="T">The PLC Tag Data Type.</typeparam>
@@ -74,6 +106,44 @@ namespace MQTTnet.Rx.TwinCAT
             }
 
             var plc = default(RxTcAdsClient)!;
+            configurePlc(plc);
+            if (plc == null)
+            {
+                throw new ArgumentNullException(nameof(plc));
+            }
+
+            return client.PublishMessage(plc.Observe<T>(plcVariable).Select(payLoad => (topic, payLoad: payLoad!.ToString()!)));
+        }
+
+        /// <summary>
+        /// Publishes the tc PLC tag.
+        /// </summary>
+        /// <typeparam name="T">The PLC Tag Data Type.</typeparam>
+        /// <param name="client">The client.</param>
+        /// <param name="topic">The topic.</param>
+        /// <param name="plcVariable">The PLC variable.</param>
+        /// <param name="configurePlc">The configure PLC.</param>
+        /// <returns>MqttClientPublishResult.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// nameof(client)
+        /// or
+        /// nameof(configurePlc)
+        /// or
+        /// nameof(plc).
+        /// </exception>
+        public static IObservable<ApplicationMessageProcessedEventArgs> PublishTcPlcTag<T>(this IObservable<IManagedMqttClient> client, string topic, string plcVariable, Action<IHashTableRx> configurePlc)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (configurePlc == null)
+            {
+                throw new ArgumentNullException(nameof(configurePlc));
+            }
+
+            var plc = default(HashTableRx)!;
             configurePlc(plc);
             if (plc == null)
             {
